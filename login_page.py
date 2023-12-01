b5739f73e619c1c0b1989524efb1939a8891173a
Sayyeda Fatima Masood <sfmasood13@gmail.com>
2023-12-01 16:41:12 +0500

    ab

--- login_page.py
+++ login_page.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
     QPushButton, QSizePolicy, QWidget)
-from psycopg2 import connect, sql
+
 
 class Ui_Login_page(object):
     def setupUi(self, Login_page):
